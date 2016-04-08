4fcf3fb6a98c353419a2a12e3aeef204563e2097
GILLIOUARD Aurélien <gillio_a@MacBook-Pro-de-GILLIOUARD.local>
2016-04-08 14:28:39 -0700

    Change FragmentActivity to AppCompatActivity

--- app/src/main/java/edu/csulb/android/androidscoretracker/LoadingActivity.java
+++ app/src/main/java/edu/csulb/android/androidscoretracker/LoadingActivity.java
@@ -1,9 +1,12 @@
 package edu.csulb.android.androidscoretracker;
 
-import android.support.v4.app.FragmentActivity;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class LoadingActivity extends FragmentActivity {
+import java.io.File;
+
+public class LoadingActivity extends AppCompatActivity {
 
     private GameSessionDatabaseManager dbSession;
     private GameDatabaseManager dbGame;
@@ -18,11 +21,11 @@ public class LoadingActivity extends FragmentActivity {
 	    //Scoreboard scoreboard = Scoreboard.newInstance(1);
         //getFragmentManager().beginTransaction().replace(R.id.main_activity, scoreboard).commit();
 
-        //GameSessionListFragment fragmentS1 = GameSessionListFragment.newInstance();
-        //getFragmentManager().beginTransaction().replace(R.id.main_activity, fragmentS1).commit();
+        GameSessionListFragment fragmentS1 = GameSessionListFragment.newInstance();
+        getFragmentManager().beginTransaction().replace(R.id.main_activity, fragmentS1).commit();
 
         //SQLiteDatabase.deleteDatabase(new File("/data/data/edu.csulb.android.androidscoretracker/ScoreTracker.db")); //-> to delete all the database
-        GameSessionDialog.showDialog(this);
+        //GameSessionDialog.showDialog(this);
         //GameDialog.showDialog(this);
     }
 
